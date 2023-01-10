6d52eaf83a65f408d63617e349542927e00c2c51
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-01-10 08:34:56 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return userRepo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id); // caso nao ache vai disparar uma exeção
+		userRepo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
